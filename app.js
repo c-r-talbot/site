e7b838d631686705621d3d9d57086f3dd46b2709
c-r-talbot <c_talbot@goron.local>
2019-01-02 22:45:21 -0500

    playing with bootstrap, changing colors, adding classes

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var calRouter = require('./routes/cal')
 
 var app = express();
 
+app.set('view engine', 'pug')
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cal', calRouter);
 
 module.exports = app;
